9032b68fa04eaa55c29486f8fe5201766ad7b9d5
putus89 <james.w.weiss@gmail.com>
2013-03-09 07:34:10 -0800

    Add FeedReadMain

---
+++ FeedReadd/src/main/java/net/brakingpoint/FeedRead/FeedReadMain.java
@@ -0,0 +1,14 @@
+package net.brakingpoint.FeedRead;
+
+public class FeedReadMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("FeedReadd starting!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
